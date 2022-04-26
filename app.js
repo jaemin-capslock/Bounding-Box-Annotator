efb592f407a9028bc9f2d01359da825149068bdd
Min <jaeminko0420@gmail.com>
2022-04-26 00:32:32 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+//onst helpers = require('./helpers');
+const app = express();
+const Jimp = require('jimp');
+//cv = require()
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/');
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+var upload = multer ({ storage: storage});
+var boxes;
+app.use(express.static(__dirname + '/public'));
+app.use('/uploads', express.static('uploads'));
+
+var myFilePath;
+
+let {PythonShell} = require('python-shell');
+//const { rsqrt } = require('@tensorflow/tfjs-core');
+//const tf = require('@tensorflow/tfjs-node');
+//const model = await tf.node.loadSavedModel('./sentimentModel', [tag], signatureKey);
+app.get('/initialdata', call_easyocr);
+
+function call_easyocr(req, res) {
+    var options = {
+        args: [myFilePath]
+    }
+    PythonShell.run('./easyocrimg.py', options, function( err, data){
+        if (err) res.send(err);
+        boxes = data;
+        res.send(data.toString())
+    });
+}
+app.post('/upload-single', upload.single('file'), function (req, res, next) {
+    console.log(JSON.stringify(req.file));
+    myFilePath = req.file.path;
+    var response = '<a href="/">Home</a><br>'
+    response += "Files uploaded successfully.<br>"
+    response += `<img src="${req.file.path}" /><br>`
+    response += '<a href="/initialdata">See data</a>'
+    return res.send(response)
+})
+
+
+app.listen(3000, () => console.log(`Listening on port 3000...`));
+
+/*
+app.post('/upload', (req, res) => {
+    // 'profile_pic' is the name of our file input field in the HTML form
+    let upload = multer({ storage: storage, fileFilter: helpers.imageFilter }).single('test_input');
+
+    upload(req, res, function(err) {
+        console.log(req.file.path);
+        // req.file contains information of uploaded file
+        // req.body contains information of text fields, if there were any
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.file) {
+            return res.send('Please select an image to upload');
+        }
+        else if (err instanceof multer.MulterError) {
+            return res.send(err);
+        }
+        else if (err) {
+            return res.send(err);
+        }
+        var myPath = "./" + req.file.path;
+        
+        console.log(myPath);
+        // Display uploaded image for user validation
+        res.send(`You have uploaded this image: <hr/><img src="${myPath}" width="500"><hr /><a href="./">Upload another image</a>`);
+    });
+});
+*/
---
+++ easyocrimg.py
@@ -0,0 +1,40 @@
+import sys
+import numpy as np
+import json
+import easyocr
+
+reader = easyocr.Reader(['en'])
+# Assume that the address of the image is given as the
+# Second arg on file call.
+
+address = sys.argv[1]
+
+result = reader.readtext(address, detail = 1)
+
+# box: Coordinates of the bounding boxes.
+def getBoxes(input):
+  boxes = []
+  for i in range(len(input)):
+    # Convert values to int, since cv2 accepts integer values
+    input[i][0][0][0], input[i][0][0][1] = int(input[i][0][0][0]), int(input[i][0][0][1])
+    input[i][0][2][0], input[i][0][2][1] = int(input[i][0][2][0]), int(input[i][0][2][1])
+    toAppend = input[i][0][0], input[i][0][2]
+    boxes.append(toAppend)
+  return boxes
+
+box = getBoxes(result)
+def readTexts(input):
+  texts = []
+  for i in range(len(input)):
+    toAppend = input[i][1]
+    texts.append(toAppend)
+  return texts
+# readText: List of texts
+readText = readTexts(result)
+
+
+
+print(json.dumps(box))
+print(json.dumps(readText))
+
+sys.stdout.flush()
